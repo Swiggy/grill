e736744339b8ac2758a4916ea45c08c599a5ef9a
Amit Shekhar <amit.shekhar@swiggy.in>
2020-06-18 02:41:04 +0530

    adds unit tests for action and try

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module bitbucket.org/swigy/grill
 
 go 1.13.6
+
+require github.com/golang/mock v1.4.3
---
+++ try_test.go
@@ -0,0 +1,35 @@
+package grill
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"bitbucket.org/swigy/grill/mock"
+	"github.com/golang/mock/gomock"
+)
+
+func TestTry(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockAssertion := mock.NewMockAssertion(ctrl)
+
+	tests := []struct {
+		name   string
+		output [][]interface{}
+	}{
+		{"NoErrors", [][]interface{}{{nil, 3}}},
+		{"ErrorInBetween", [][]interface{}{{nil, 1}, {fmt.Errorf("error"), 1}, {nil, 3}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var calls []*gomock.Call
+			for _, o := range tt.output {
+				calls = append(calls, mockAssertion.EXPECT().Assert().Return(o[0]).Times(o[1].(int)))
+			}
+			gomock.InOrder(calls...)
+			Try(time.Second, 3, mockAssertion).Assert()
+		})
+	}
+}
