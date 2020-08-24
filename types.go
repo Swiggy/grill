fd222adefc0aedb172d8eb3aa0a5e4d7595df117
Amit Shekhar <amit.shekhar@swiggy.in>
2020-08-25 02:01:18 +0530

    makes interface functions

--- types.go
+++ types.go
@@ -4,10 +4,28 @@ type Stub interface {
 	Stub() error
 }
 
+type StubFunc func() error
+
+func (fn StubFunc) Stub() error {
+	return fn()
+}
+
 type Assertion interface {
 	Assert() error
 }
 
+type AssertionFunc func() error
+
+func (fn AssertionFunc) Assert() error {
+	return fn()
+}
+
 type Cleaner interface {
 	Clean() error
+}
+
+type CleanerFunc func() error
+
+func (fn CleanerFunc) Clean() error {
+	return fn()
 }
