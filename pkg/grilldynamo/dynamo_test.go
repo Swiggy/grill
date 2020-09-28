86b589e75e08b8ffcdcb386d6e75e30b3b222c5d
Amit Shekhar <amit.shekhar@swiggy.in>
2020-09-28 05:35:19 +0530

    Add dynamo put and delete methods

--- pkg/grilldynamo/dynamo_test.go
+++ pkg/grilldynamo/dynamo_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
 	"bitbucket.org/swigy/grill"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
@@ -18,25 +20,29 @@ type testItem struct {
 	Name         testVal
 }
 
-var testTableRequest = &dynamodb.CreateTableInput{
-	AttributeDefinitions: []*dynamodb.AttributeDefinition{
-		{
-			AttributeType: aws.String(dynamodb.ScalarAttributeTypeS),
-			AttributeName: aws.String("PartitionKey"),
+var (
+	testTableRequest = &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeType: aws.String(dynamodb.ScalarAttributeTypeS),
+				AttributeName: aws.String("PartitionKey"),
+			},
 		},
-	},
-	KeySchema: []*dynamodb.KeySchemaElement{
-		{
-			KeyType:       aws.String(dynamodb.KeyTypeHash),
-			AttributeName: aws.String("PartitionKey"),
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				KeyType:       aws.String(dynamodb.KeyTypeHash),
+				AttributeName: aws.String("PartitionKey"),
+			},
 		},
-	},
-	TableName: aws.String("testTable"),
-	ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
-		ReadCapacityUnits:  aws.Int64(10),
-		WriteCapacityUnits: aws.Int64(10),
-	},
-}
+		TableName: aws.String("testTable"),
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(10),
+		},
+	}
+
+	testDBItem, _ = dynamodbattribute.MarshalMap(&testItem{PartitionKey: "test", Name: testVal{Value: "test"}})
+)
 
 func Test_GrillDynamo(t *testing.T) {
 	helper := &Dynamo{}
@@ -65,6 +71,27 @@ func Test_GrillDynamo(t *testing.T) {
 				}, testItem{PartitionKey: "3", Name: testVal{Value: "three"}}),
 			},
 			Cleaners: []grill.Cleaner{
+				helper.DeleteTable(tableName),
+			},
+		},
+		{
+			Name: "TestDynamo_PutItem",
+			Stubs: []grill.Stub{
+				helper.CreateTable(testTableRequest),
+				helper.PutItem(tableName, testDBItem),
+			},
+			Action: func() interface{} {
+				return nil
+			},
+			Assertions: []grill.Assertion{
+				helper.AssertScanCount(&dynamodb.ScanInput{TableName: aws.String(tableName)}, 1),
+				helper.AssertItem(&dynamodb.GetItemInput{
+					TableName: aws.String(tableName),
+					Key:       map[string]*dynamodb.AttributeValue{"PartitionKey": {S: aws.String("test")}},
+				}, testItem{PartitionKey: "test", Name: testVal{Value: "test"}}),
+			},
+			Cleaners: []grill.Cleaner{
+				helper.DeleteItem(tableName, map[string]*dynamodb.AttributeValue{"PartitionKey": {S: aws.String("test")}}),
 				helper.DeleteTable(tableName),
 			},
 		},
