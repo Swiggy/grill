6d921e0c7eb4dff5ea9955ac2a9f4170fcbdff80
Aditya Singh | DASH SCM <101247790+AdiXeno@users.noreply.github.com>
2024-10-10 00:55:59 +0530

    init

--- pkg/grillgrpc/assertions.go
+++ pkg/grillgrpc/assertions.go
@@ -10,7 +10,7 @@ func (gg *GRPC) AssertCount(request Request, expectedCount int) grill.Assertion 
 	return grill.AssertionFunc(func() error {
 		got := gg.recorder.count(&request)
 		if got != expectedCount {
-			return fmt.Errorf("invalid number of requests, got=%v, want=%v", got, expectedCount)
+			return fmt.Errorf("invalid number of requests, got=%v, want=%v for method=%v", got, expectedCount, request.Method)
 		}
 		return nil
 	})
