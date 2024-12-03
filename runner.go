c007bab4ec093ec68f3a6d7824239df0bb66b5bf
Udayabharathi <94507000+udayabharathi-t@users.noreply.github.com>
2024-12-03 11:33:54 +0530

    Update runner.go

--- runner.go
+++ runner.go
@@ -16,7 +16,6 @@ func RunParallel(t *testing.T, testCases []TestCase) {
 	wg.Add(len(testCases))
 
 	for _, testCase := range testCases {
-		testCase := testCase
 		go func(tt TestCase, wg *sync.WaitGroup) {
 			defer wg.Done()
 
